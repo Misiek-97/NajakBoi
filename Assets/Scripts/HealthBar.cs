72c4d13c989b32a948f98dedb4b592864151dd7b
Misiek-97 <51755606+Misiek-97@users.noreply.github.com>
2023-11-18 00:16:53 +0100

    Health Bar, HUD, GameOver screen + Fixes

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,21 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-
-    public Slider slider;
-
-    public void SetMaxHealth(int health)
+    public Image healthBarFill;
+    public float drainSpeed;
+    public CharacterController character;
+    
+    // Update is called once per frame
+    void Update()
     {
-        slider.maxValue = health;
-        slider.value = health;
+        var currentHealth = character.currentHealth;
+        // Example: Drain the health over time
+        if (currentHealth > 0)
+        {
+            currentHealth -= Time.deltaTime * drainSpeed; // Adjust the speed of draining
+
+            // Ensure that the health doesn't go below 0
+            currentHealth = Mathf.Max(currentHealth, 0.0f);
+
+            // Calculate health percentage
+            float healthPercentage = currentHealth / character.maxHealth;
+
+            // Lerp the health bar fill amount
+            SetHealthBarFill(Mathf.Lerp(healthBarFill.fillAmount, healthPercentage, Time.deltaTime * 5.0f));
+        }
+        else
+        {
+            // Lerp the health bar fill amount
+            SetHealthBarFill(Mathf.Lerp(healthBarFill.fillAmount, 0, Time.deltaTime * 5.0f));
+            
+        }
     }
-
-    public void SetHealth(int health)
+    // Set the fill amount of the health bar based on the health percentage
+    void SetHealthBarFill(float percentage)
     {
-        slider.value = health;
-    }    
+        healthBarFill.fillAmount = percentage;
+    }
 }
