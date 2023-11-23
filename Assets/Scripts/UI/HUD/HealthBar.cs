882822005d2697155ec5fcf94361ff7a77168d4d
Grzesiczeeek <138173565+Grzesiczeeek@users.noreply.github.com>
2023-11-23 16:16:46 +0100

    Bar + Turns + PlayerID
    - Created Bar base class
    - HealthBar now inhertis from Bar
    - Added PlayerId and use that for Turns
    - Made separate prefab for Opponent
    - Updated PlayerController

--- Assets/Scripts/UI/HUD/HealthBar.cs
+++ Assets/Scripts/UI/HUD/HealthBar.cs
@@ -1,40 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class HealthBar : MonoBehaviour
+public class HealthBar : Bar
 {
-    public Image healthBarFill;
-    public float drainSpeed;
     public PlayerController player;
-    
-    // Update is called once per frame
-    void Update()
+
+
+    public void UpdateHealth()
     {
-        var currentHealth = player.currentHealth;
-        // Example: Drain the health over time
-        if (currentHealth > 0)
-        {
-            currentHealth -= Time.deltaTime * drainSpeed; // Adjust the speed of draining
-
-            // Ensure that the health doesn't go below 0
-            currentHealth = Mathf.Max(currentHealth, 0.0f);
-
-            // Calculate health percentage
-            float healthPercentage = currentHealth / player.maxHealth;
-
-            // Lerp the health bar fill amount
-            SetHealthBarFill(Mathf.Lerp(healthBarFill.fillAmount, healthPercentage, Time.deltaTime * 5.0f));
-        }
-        else
-        {
-            // Lerp the health bar fill amount
-            SetHealthBarFill(Mathf.Lerp(healthBarFill.fillAmount, 0, Time.deltaTime * 5.0f));
-            
-        }
+        amount = player.currentHealth;
+        maxAmount = player.maxHealth;
     }
-    // Set the fill amount of the health bar based on the health percentage
-    void SetHealthBarFill(float percentage)
-    {
-        healthBarFill.fillAmount = percentage;
-    }
+
 }
