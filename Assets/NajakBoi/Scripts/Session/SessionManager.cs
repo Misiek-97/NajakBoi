68aa8dda8720abd9d76efab25a65e6621353de40
Misiek-97 <51755606+Misiek-97@users.noreply.github.com>
2023-11-27 22:20:43 +0100

    Session Manager + TheLab + Resources Serialization

--- Assets/NajakBoi/Scripts/Session/SessionManager.cs
+++ Assets/NajakBoi/Scripts/Session/SessionManager.cs
@@ -1,5 +1,6 @@
 using System;
-using NajakBoi.Scripts.Systems.Economy;
+using System.IO;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace NajakBoi.Scripts.Session
@@ -7,25 +8,76 @@ namespace NajakBoi.Scripts.Session
     public class SessionManager : MonoBehaviour
     {
         private static DateTime _lastSave = DateTime.Now;
+        public static PlayerData PlayerData;
+        public static string ResourcesPath => Path.Combine(Application.persistentDataPath, "Resources.json");
+
+        public static SessionManager Session;
         
+
+        private void Awake()
+        {
+            if (Session)
+            {
+                Debug.Log("Session already exists!");
+                Destroy(gameObject);
+                return;
+            }
+            
+            Session = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        
+        
+
+        public void OnDestroy()
+        {
+            if (Session != this) return;
+            
+            Session = null;
+            Save();
+        }
+
         private void Start()
         {
-            GlobalSession.Save();
-            
-            GlobalSession.Session.Player.GainResource(ResourceType.Coins, 10);
+            if (File.Exists(ResourcesPath))
+            {
+                PlayerData = new PlayerData();
+                try
+                {
+                    PlayerData.LoadPlayerData();
+                }
+                catch (Exception ex)
+                {
+                    Debug.Log($"Save corrupt at {ResourcesPath}!");
+                    Debug.LogError(ex);
+                }
+            }
+            else
+            {
+                NewSession();
+            }
+        }
+
+        public void Save()
+        {
+            PlayerData.SavePlayerData();
+        }
+
+
+        private static void NewSession()
+        {
+            PlayerData = new PlayerData();
+            PlayerData.PopulateResourcesDictionary();
+            Debug.Log("NEW PLAY SESSION");
         }
         private void Update()
         {
             if (_lastSave < DateTime.Now.AddMinutes(-1))
             {
                 _lastSave = DateTime.Now;
-                GlobalSession.Save();
+                Save();
             }
         }
 
-        public void OnDestroy()
-        {
-            GlobalSession.Save();
-        }
     }
 }
